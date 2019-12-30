a273c0e673d8ff7f09b9de0b5251964c213f369e
thaparahul <connectthapa84@gmail.com>
2019-12-29 23:53:00 -0600

    Updates

---
+++ precache-manifest.f047b41d619eaaa59ab96d8dfc85260e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afde7122f8e5de828adf8f9a03892690",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "4bc593ce801caebd2b66",
+    "url": "/MyPortfolio/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "8f6068810f6492401d1e",
+    "url": "/MyPortfolio/static/js/2.14d4675f.chunk.js"
+  },
+  {
+    "revision": "4bc593ce801caebd2b66",
+    "url": "/MyPortfolio/static/js/main.a43f976c.chunk.js"
+  },
+  {
+    "revision": "bb5628a3be746b98ea29",
+    "url": "/MyPortfolio/static/js/runtime~main.d060ed7c.js"
+  }
+]);
